ffd5a3446a6210aa8bb1ce1dfeeaec3c3081d657
Kethan Kumar <kethan.kumar@oyster.io>
2022-09-02 16:38:15 +0530

    Initial Commit

---
+++ Sources/CustomFramework/LMSManager.swift
@@ -0,0 +1,10 @@
+public struct LMSManager {
+    public static var shared = LMSManager()
+
+    private init() {
+    }
+
+    public func testFramework() {
+        print("Test Framework")
+    }
+}
